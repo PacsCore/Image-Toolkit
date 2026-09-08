b117b5c79095fa0a11ae5642f6816304c8941f54
PacsCore <enrique.achacoso@proton.me>
2026-09-08 22:39:41 +0200

    Set up Script

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    
+const imageInput = document.getElementById('imageInput');
+const Canvas = document.getElementById('imageCanvas');
+const ctx = Canvas.getContext('2d');
+
+imageInput.addEventListener('change', function() {
+    console.log('file selected');
+});
